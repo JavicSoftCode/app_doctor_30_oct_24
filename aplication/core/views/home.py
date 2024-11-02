88de2dfa0d065b94c00a260eb500548c56b56974
Ing-Javier-Sistem <145406699+Ing-Javier-Sistem@users.noreply.github.com>
2024-11-02 02:42:35 -0500

    time messages erros

--- aplication/core/views/home.py
+++ aplication/core/views/home.py
@@ -9,5 +9,5 @@ class HomeTemplateView(TemplateView):
   def get_context_data(self, **kwargs):
     context = super().get_context_data(**kwargs)
     context = {"title1": "SaludSync", "title2": "Sistema Medico", "can_paci": Paciente.cantidad_pacientes()}
-    print(context["can_paci"])
+    # print(context["can_paci"])
     return context
