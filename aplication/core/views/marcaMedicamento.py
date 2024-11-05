8a7f45ba1db7e74e854f79b503e849d90adfdc10
Ing-Javier-Sistem <145406699+Ing-Javier-Sistem@users.noreply.github.com>
2024-11-05 11:16:15 -0500

    The software is finished, but it needs code cleanup and improvements.

--- aplication/core/views/marcaMedicamento.py
+++ aplication/core/views/marcaMedicamento.py
@@ -4,22 +4,22 @@ from django.http import JsonResponse
 from django.urls import reverse_lazy
 from django.views.generic import CreateView, ListView, UpdateView, DeleteView, DetailView
 
-from aplication.core.forms.specialty import SpecialtyForm
-from aplication.core.models import Especialidad
+from aplication.core.forms.marcaMedicamento import MarcaMedicamentoForm
+from aplication.core.models import MarcaMedicamento
 from doctor.utils import save_audit
 
 
-class SpecialtyListView(ListView):
-  template_name = "core/specialty/list.html"
-  model = Especialidad
-  context_object_name = 'especialidades'
+class MarcaMedicamentoListView(ListView):
+  template_name = "core/marcaMedicamento/list.html"
+  model = MarcaMedicamento
+  context_object_name = 'marcas'
   paginate_by = 5
   query = None
 
   def get_queryset(self):
     self.query = Q()
     q1 = self.request.GET.get('q')  # Texto de búsqueda
-    specialty = self.request.GET.get('especialidad')  # Estado activo o inactivo
+    activo = self.request.GET.get('activo')  # Estado activo o inactivo
 
     if q1:
       if q1.isdigit():
@@ -29,39 +29,39 @@ class SpecialtyListView(ListView):
         # Filtra por nombre que contenga el valor ingresado en 'q'
         self.query.add(Q(nombre__icontains=q1), Q.AND)
 
-    if specialty in ["True", "False"]:
+    if activo in ["True", "False"]:
       # Filtra por el valor booleano de activo
-      is_active = specialty == "True"  # Convierte a booleano
+      is_active = activo == "True"  # Convierte a booleano
       self.query.add(Q(activo=is_active), Q.AND)
 
     return self.model.objects.filter(self.query).order_by('nombre')
 
   def get_context_data(self, **kwargs):
     context = super().get_context_data(**kwargs)
-    context['title1'] = "Listado de Especialidades"
-    context['title2'] = "Nueva Especialidad"
+    context['title1'] = "Listado de Marcas de Medicamentos"
+    context['title2'] = "Nueva Marca de Medicamento"
     return context
 
 
-class SpecialtyCreateView(CreateView):
-  model = Especialidad
-  template_name = 'core/specialty/form.html'
-  form_class = SpecialtyForm
-  success_url = reverse_lazy('core:specialty_list')
+class MarcaMedicamentoCreateView(CreateView):
+  model = MarcaMedicamento
+  template_name = 'core/marcaMedicamento/form.html'
+  form_class = MarcaMedicamentoForm
+  success_url = reverse_lazy('core:marcaMedicamento_list')
 
   def get_context_data(self, **kwargs):
     context = super().get_context_data()
-    context['title1'] = 'Crear Especialidad'
-    context['grabar'] = 'Grabar Especialidad'
+    context['title1'] = 'Crear Marca Medicamento'
+    context['grabar'] = 'Grabar Marca Medicamento'
     context['back_url'] = self.success_url
     return context
 
   def form_valid(self, form):
     # print("entro al form_valid")
     response = super().form_valid(form)
-    specialty = self.object
-    save_audit(self.request, specialty, action='A')
-    messages.success(self.request, f"Éxito al crear la Especialidad {specialty.nombre}.")
+    marcaM = self.object
+    save_audit(self.request, marcaM, action='A')
+    messages.success(self.request, f"Éxito al crear la Marca Medicamento {marcaM.nombre}.")
     return response
 
   def form_invalid(self, form):
@@ -70,24 +70,24 @@ class SpecialtyCreateView(CreateView):
     return self.render_to_response(self.get_context_data(form=form))
 
 
-class SpecialtyUpdateView(UpdateView):
-  model = Especialidad
-  template_name = 'core/specialty/form.html'
-  form_class = SpecialtyForm
-  success_url = reverse_lazy('core:specialty_list')
+class MarcaMedicamentoUpdateView(UpdateView):
+  model = MarcaMedicamento
+  template_name = 'core/marcaMedicamento/form.html'
+  form_class = MarcaMedicamentoForm
+  success_url = reverse_lazy('core:marcaMedicamento_list')
 
   def get_context_data(self, **kwargs):
     context = super().get_context_data()
-    context['title1'] = 'Actualizar Especialidad'
-    context['grabar'] = 'Actualizar Especialidad'
+    context['title1'] = 'Actualizar Marca Medicamento'
+    context['grabar'] = 'Actualizar Marca Medicamento'
     context['back_url'] = self.success_url
     return context
 
   def form_valid(self, form):
     response = super().form_valid(form)
-    specialty = self.object
-    save_audit(self.request, specialty, action='M')
-    messages.success(self.request, f"Éxito al Modificar la Especialidad {specialty.nombre}.")
+    marcaM = self.object
+    save_audit(self.request, marcaM, action='M')
+    messages.success(self.request, f"Éxito al Modificar la Marca Medicamento {marcaM.nombre}.")
     print("mande mensaje")
     return response
 
@@ -97,23 +97,23 @@ class SpecialtyUpdateView(UpdateView):
     return self.render_to_response(self.get_context_data(form=form))
 
 
-class SpecialtyDeleteView(DeleteView):
-  model = Especialidad
-  success_url = reverse_lazy('core:specialty_list')
+class MarcaMedicamentoDeleteView(DeleteView):
+  model = MarcaMedicamento
+  success_url = reverse_lazy('core:marcaMedicamento_list')
 
   def get_context_data(self, **kwargs):
     context = super().get_context_data(**kwargs)
-    context['grabar'] = 'Eliminar Especialidad'
-    context['description'] = f"¿Desea Eliminar la Especialidad: {self.object.nombre}?"
+    context['grabar'] = 'Eliminar Marca Medicamento'
+    context['description'] = f"¿Desea Eliminar la Marca Medicamento: {self.object.nombre}?"
     return context
 
   def delete(self, request, *args, **kwargs):
     self.object = self.get_object()
-    specialty_name = self.object.nombre  # Guardamos el nombre de la especialidad
+    specialty_name = self.object.nombre  # Guardamos el nombre de la
     # Guarda la auditoría de la eliminación
     save_audit(self.request, self.object, action='E')
 
-    success_message = f"Éxito al eliminar lógicamente la Especialidad {specialty_name}."
+    success_message = f"Éxito al eliminar lógicamente la Marca Medicamento {specialty_name}."
     messages.success(self.request, success_message)
 
     # Cambiar el estado de eliminado lógico (si es necesario)
@@ -143,18 +143,18 @@ class SpecialtyDeleteView(DeleteView):
 #     return super().delete(request, *args, **kwargs)
 
 
-class SpecialtyDetailView(DetailView):
-  model = Especialidad
+class MarcaMedicamentoDetailView(DetailView):
+  model = MarcaMedicamento
   extra_context = {
-    "detail": "Detalles de la Especialidad"
+    "detail": "Detalles de la Marca Medicamento"
   }
 
   def get(self, request, *args, **kwargs):
-    specialty = self.get_object()
+    marcaM = self.get_object()
     data = {
-      'id': specialty.id,
-      'nombre': specialty.nombre,
-      'descripcion': specialty.descripcion,
-      'activo': specialty.activo,
+      'id': marcaM.id,
+      'nombre': marcaM.nombre,
+      'descripcion': marcaM.descripcion,
+      'activo': marcaM.activo,
     }
     return JsonResponse(data)
